0e8aa66c52c79e7801b81349cb692c67c923298f
Sam Pillsworth <sam.pillsworth@affinio.com>
2016-12-15 09:57:48 -0400

    adds directory for training slim on custom dataset:

---
+++ slim_on_customdata/train_inception_on_testset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# This script performs the following operations:
+# 1. Downloads the Cifar10 dataset
+# 2. Trains a CifarNet model on the Cifar10 training set.
+# 3. Evaluates the model on the Cifar10 testing set.
+#
+# Usage:
+# cd slim
+# ./scripts/train_cifar_net_on_mnist.sh
+
+# Where the checkpoint and logs will be saved to.
+TRAIN_DIR=/tmp/testset_training
+
+# Where the dataset is saved to.
+DATASET_DIR=/Users/work/Downloads/images_2016_08/train/test_output
+
+# Run training.
+python train_image_classifier.py \
+  --train_dir=${TRAIN_DIR} \
+  --dataset_name=testset \
+  --dataset_split_name=train \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v3 \
+  --preprocessing_name=inception \
+  --max_number_of_steps=100000 \
+  --batch_size=32 \
+  --save_interval_secs=120 \
+  --save_summaries_secs=120 \
+  --log_every_n_steps=100 \
+  --optimizer=sgd \
+  --learning_rate=0.1 \
+  --learning_rate_decay_factor=0.16 \
+  --num_epochs_per_decay=20 \
+  --weight_decay=0.04
+
+# Run evaluation.
+# python eval_image_classifier.py \
+#   --checkpoint_path=${TRAIN_DIR} \
+#   --eval_dir=${TRAIN_DIR} \
+#   --dataset_name=inception \
+#   --dataset_split_name=validation \
+#   --dataset_dir=${DATASET_DIR} \
+#   --model_name=cifarnet
